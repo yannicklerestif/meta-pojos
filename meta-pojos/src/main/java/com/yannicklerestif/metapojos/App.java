2d8a6567f04c71417f7cbe68ce38ced98ce0b7d4
yannick <yannick1976_git@yahoo.fr>
2015-03-01 14:08:40 +0100

    init

---
+++ meta-pojos/src/main/java/com/yannicklerestif/metapojos/App.java
@@ -0,0 +1,13 @@
+package com.yannicklerestif.metapojos;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
