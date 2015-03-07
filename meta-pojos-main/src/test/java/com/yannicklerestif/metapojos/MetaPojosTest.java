b5c92af2e7067c03718e06305f38752be17b6b95
yannick <yannick1976_git@yahoo.fr>
2015-03-07 01:16:28 +0100

    little things

--- meta-pojos-main/src/test/java/com/yannicklerestif/metapojos/MetaPojosTest.java
+++ meta-pojos-main/src/test/java/com/yannicklerestif/metapojos/MetaPojosTest.java
@@ -1,12 +1,13 @@
 package com.yannicklerestif.metapojos;
 
 
+
 public class MetaPojosTest {
 	public static void main(String[] args) throws Exception {
 		MetaPojos mp = MetaPojos.start();
 		mp.readClasses("/home/yannick/Projets/meta-pojos/meta-pojos-test-classes/bin");
 //		mp.readClasses("/usr/lib/java/jdk1.8.0_31/jre/lib/rt.jar");
-		mp.allClasses().getMethods().getCalls().print();
+		mp.allClasses().getMethods().getCallsTo();
 //		mp.singleClass("test.model.hierarchy.MyInterface").getMethods().getCallsTo().print();
 	}
 }
