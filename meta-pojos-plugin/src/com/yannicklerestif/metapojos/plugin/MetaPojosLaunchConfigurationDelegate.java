4bddb7e830fa0f94ad3ca944f714319c39882bc6
yannick <yannick1976_git@yahoo.fr>
2015-03-27 21:13:40 +0100

    replaced hard-coded main type and project output directory with the ones from the launch config

--- meta-pojos-plugin/src/com/yannicklerestif/metapojos/plugin/MetaPojosLaunchConfigurationDelegate.java
+++ meta-pojos-plugin/src/com/yannicklerestif/metapojos/plugin/MetaPojosLaunchConfigurationDelegate.java
@@ -1,46 +1,65 @@
 package com.yannicklerestif.metapojos.plugin;
 
 import java.io.File;
+import java.io.IOException;
 import java.lang.reflect.Method;
 import java.net.URL;
 import java.net.URLClassLoader;
 
+import org.eclipse.core.resources.IFolder;
+import org.eclipse.core.resources.IWorkspaceRoot;
+import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.debug.core.ILaunch;
 import org.eclipse.debug.core.ILaunchConfiguration;
 import org.eclipse.debug.core.model.LaunchConfigurationDelegate;
+import org.eclipse.jdt.core.IJavaProject;
+import org.eclipse.jdt.core.JavaCore;
+import org.eclipse.jdt.launching.IJavaLaunchConfigurationConstants;
 
 public class MetaPojosLaunchConfigurationDelegate extends LaunchConfigurationDelegate {
 
 	public static final String LAUNCHER_ID = "com.yannicklerestif.metapojos.Launcher";
-	
+
 	@Override
-	public void launch(ILaunchConfiguration configuration, String mode,
-			ILaunch launch, IProgressMonitor monitor) throws CoreException {
+	public void launch(ILaunchConfiguration configuration, String mode, ILaunch launch, IProgressMonitor monitor)
+			throws CoreException {
 		System.out.println();
 		System.out.println("Launching -------------------------------");
-		for(String key : configuration.getAttributes().keySet()) {
-			Object value = configuration.getAttributes().get(key);
-			System.out.println(key + " => " + value);
-		}
-		System.out.println("---------------------");
+
+		String mainType = (String) configuration.getAttributes().get(
+				IJavaLaunchConfigurationConstants.ATTR_MAIN_TYPE_NAME);
+		String projectName = (String) configuration.getAttributes().get(
+				IJavaLaunchConfigurationConstants.ATTR_PROJECT_NAME);
+		IJavaProject project = JavaCore.create(ResourcesPlugin.getWorkspace().getRoot().getProject(projectName));
+		
+		IPath path = project.getOutputLocation();
+		IWorkspaceRoot root = ResourcesPlugin.getWorkspace().getRoot();
+		IFolder folder = root.getFolder(path);
+		File outputLocation = folder.getLocation().toFile();
 		
 		Class clazz = null;
-		
-        //TODO replace hard-coded strings by values from config
+		URLClassLoader cl = null;
+
 		try {
-            URL url = new File("C:/eclipse_workspaces/runtime-EclipseApplication/TestProject/bin").toURI().toURL();
-			ClassLoader cl = new URLClassLoader(new URL[]{url}, null);
-			clazz = cl.loadClass("com.yannick.MyOtherClass");
-			
-			//TODO passing args to the methods might have some interest 
+			URL url = outputLocation.toURI().toURL();
+			cl = new URLClassLoader(new URL[] { url }, null);
+			clazz = cl.loadClass(mainType);
 			Method mainMethod = clazz.getMethod("main", String[].class);
-			mainMethod.invoke(null, new Object[]{null});
+			mainMethod.invoke(null, new Object[] { null });
+			cl.close();
 		} catch (Exception e) {
+			try {
+				cl.close();
+			} catch (IOException e1) {
+				//we do not want to hide the original exception
+				e1.printStackTrace();
+			}
 			throw new RuntimeException(e);
 		}
-		
+
 	}
 
 }
