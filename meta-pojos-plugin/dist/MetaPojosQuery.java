7e736345fee5f268e2ad6ea7512a8f32f3d0cf7d
yannick <yannick1976_git@yahoo.fr>
2015-04-04 23:34:27 +0200

    gitignore

--- meta-pojos-plugin/dist/MetaPojosQuery.java
+++ meta-pojos-plugin/dist/MetaPojosQuery.java
@@ -5,11 +5,8 @@ import com.yannicklerestif.metapojos.MetaPojos;
 public class MetaPojosQuery {
 	public static void main(String[] args) throws Exception {
 		MetaPojos mp = MetaPojos.start();
-		mp.readClasses("/home/yannick/Projets/meta-pojos/meta-pojos-samples/src/classesLocations.txt");
-//		mp.readClasses("C:/eclipse_workspaces/Java/meta-pojos/meta-pojos-samples/src/classesLocations-windows.txt");
-//		mp.readClasses("/usr/lib/java/jdk1.8.0_31/jre/lib/rt.jar");
-//		mp.singleClass(ClassBean.class.getName()).getMethods().matches("<init>").getCallsTo().print();
-//		mp.singleClass("test.model.hierarchy.MyInterface").getMethods().getCallsTo().print();
+		//		mp.singleClass(ClassBean.class.getName()).getMethods().matches("<init>").getCallsTo().print();
+		//		mp.singleClass("test.model.hierarchy.MyInterface").getMethods().getCallsTo().print();
 		MetaPojos.getConsole().println(mp.allClasses().stream().count());
 	}
 }
